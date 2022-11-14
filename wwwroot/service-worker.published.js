6b9f5f99b547181948182d4b42b1c229e9f9f251
MichalMalenda <michalmalenda@gmail.com>
2022-11-14 07:38:41 +0100

    s

--- wwwroot/service-worker.published.js
+++ wwwroot/service-worker.published.js
@@ -21,7 +21,7 @@ async function onInstall(event) {
         .map(asset => new Request(asset.url, { integrity: asset.hash }));
 
     // Also cache authentication configuration
-    assetsRequests.push(new Request('_configuration/MichalMalenda.github.io.Client'));
+    assetsRequests.push(new Request('_configuration/'));
 
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
