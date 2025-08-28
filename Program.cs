f41bb12fb370af07f6a623993bd47b446e0cb7af
Michał Malenda <michalmalenda@gmail.com>
2025-08-28 23:25:46 +0200

    up

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MtgApiManager.Lib.Service;
+using Microsoft.JSInterop;
 
 namespace MichalMalenda.github.io
 {
@@ -16,7 +17,18 @@ namespace MichalMalenda.github.io
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<MtgService>();
             IMtgServiceProvider serviceProvider = new MtgServiceProvider();
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            var nav = host.Services.GetRequiredService<Microsoft.AspNetCore.Components.NavigationManager>();
+            var js = host.Services.GetRequiredService<IJSRuntime>();
+            var uri = new Uri(nav.Uri);
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            var originalPath = query["path"];
+            if (!string.IsNullOrEmpty(originalPath))
+            {
+                var newUrl = new Uri(new Uri(nav.BaseUri), originalPath).ToString();
+                await js.InvokeVoidAsync("eval", $"window.history.replaceState(null, null, '{newUrl}');");
+            }
+            await host.RunAsync();
         }
     }
 }
